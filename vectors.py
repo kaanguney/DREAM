d09e74b0ee07815521f93bfe1b7f3b10b835f092
kaanguney <kaanguney@sabanciuniv.edu>
2022-06-29 14:50:40 +0200

    store expressions for the model predictions

--- vectors.py
+++ vectors.py
@@ -1,6 +1,7 @@
 from tqdm import tqdm
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
 
 # kmerization might be an expensive process
 MEMORY_MODES = [15, 20, 25]
@@ -45,16 +46,16 @@ def kmerize(filename="./data/train_sequences.txt", stride=1, size=4):
            int size: size of the kmer.
     :return:
             pd.DataFrame: Pandas object with row pair as sequence and the corresponding kmer.
+            list exprs: expressions of promoter sequences.
     """
-    sequences = []
-    kmers = []
+    sequences, kmers, exprs = [], [], []
     with open(filename) as f:
         for i, line in enumerate(tqdm(f.readlines())):
             if i == cutoff:
                 print("Processing, done.")
                 break
             seq, expr = line.split('\t')
-            expr = float(expr.replace("\n", ""))
+            exprs.append(float(expr.replace("\n", "")))
             seq = remove_suffix(remove_prefix(seq, PREFIX), SUFFIX)
             try:
                 # suboptimal solution to padding
@@ -66,7 +67,8 @@ def kmerize(filename="./data/train_sequences.txt", stride=1, size=4):
             except IndexError:
                 pass
     database = pd.DataFrame({'sequence': sequences, "kmers": kmers})
-    return database
+    exprs = np.array(exprs, dtype=np.float16)
+    return database, exprs
 
 def tokenize(size):
     """
@@ -75,10 +77,11 @@ def tokenize(size):
            int size: length of each kmer.
     :return:
             pd.DataFrame: Pandas object consisting of kmer frequencies indexed by sequences.
+            list exprs: expressions of promoter sequences.
     """
-    database = kmerize(size=size)
+    database, exprs = kmerize(size=size)
     vectorizer = CountVectorizer()
     vectorizer.fit(database["kmers"])
     data = vectorizer.transform(database["kmers"])
     vectors = pd.DataFrame(data.toarray(), database["sequence"].values, vectorizer.get_feature_names())
-    return vectors
+    return vectors, exprs
